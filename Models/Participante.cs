b38f07eb7544963eccceebf4b93f189272391de9
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-03 00:00:00 -0300

    Adicionado modelo de 'Participante'

---
+++ Models/Participante.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace minhaApiWeb.Models
+{
+    public class Participante
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="Esse campo é obrigatório")]
+        [MaxLength(60, ErrorMessage="Esse campo deve conter entre 3 e 60 caracteres")]
+        [MinLength(3, ErrorMessage="Esse campo deve conter entre 3 e 60 caracteres")]
+        public string Nome { get; set; }
+    }
+}
