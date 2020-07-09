aca084e813c36e2f05a4b099a78a987e96c6a57a
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-09 00:00:00 -0300

    Adicionado modelo e controlador de 'Convidado'

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,15 +1,13 @@
-using Microsoft.EntityFrameworkCore;
 using minhaApiWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace minhaApiWeb.Data
-{
-    public class DataContext : DbContext
-    {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
+namespace minhaApiWeb.Data {
+    public class DataContext : DbContext {
+        public DataContext (DbContextOptions<DataContext> options) : base (options) {
 
         }
 
         public DbSet<Participante> Participantes { get; set; }
+        public DbSet<Convidado> Convidados { get; set; }
     }
 }
