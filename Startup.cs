4905176a1524269452f35b75d57e76839b35768d
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-09 15:22:02 -0300

    Adicionando o DbContext ao arquivo 'Startup.cs'

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,8 @@ namespace minhaApiWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("dados"));
+            services.AddScoped<DataContext, DataContext>();
             services.AddControllers();
         }
 
