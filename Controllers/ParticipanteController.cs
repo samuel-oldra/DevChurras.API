7b484069c5968f1b65476342e8f752b36cd4dca4
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-09 15:44:28 -0300

    Adicionado controlador de 'Participante'

---
+++ Controllers/ParticipanteController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using minhaApiWeb.Data;
+using minhaApiWeb.Models;
+
+namespace minhaApiWeb.Controllers
+{
+    [ApiController]
+    [Route("participantes")]
+    public class ParticipanteController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Participante>>> Get([FromServices] DataContext context)
+        {
+            var participantes = await context.Participantes.ToListAsync();
+            return participantes;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<Participante>> Post(
+            [FromServices] DataContext context,
+            [fromBody] Participante model)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Participantes.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+            }   
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
