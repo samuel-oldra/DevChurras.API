493899f473761b86d42b6a1a6786d1f2e05ac0aa
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-11 19:00:00 -0300

    Verifica se existe um participante e tem um convidado #5

--- Controllers/ConvidadoController.cs
+++ Controllers/ConvidadoController.cs
@@ -12,13 +12,29 @@ namespace minhaApiWeb.Controllers {
         [HttpGet]
         [Route ("")]
         public async Task<ActionResult<List<Convidado>>> Get ([FromServices] DataContext context) {
-            var convidados = await context.Convidados.Include (x => x.Participante).ToListAsync ();
+            var convidados = await context.Convidados
+                .Include (p => p.Participante)
+                .ToListAsync ();
             return convidados;
         }
 
         [HttpPost]
         [Route ("")]
         public async Task<ActionResult<Convidado>> Post ([FromServices] DataContext context, [FromBody] Convidado model) {
+            // Participante não encontrado
+            var hasParticipante = await context.Participantes
+                .AsNoTracking ()
+                .FirstOrDefaultAsync (x => x.ParticipanteId == model.ParticipanteId);
+            if (hasParticipante == null)
+                return BadRequest ("Participante não encontrado");
+
+            // Participante já possui um convidado
+            var hasConvidado = await context.Convidados
+                .AsNoTracking ()
+                .FirstOrDefaultAsync (x => x.ParticipanteId == model.ParticipanteId);
+            if (hasConvidado != null)
+                return BadRequest ("Participante já possui um convidado");
+
             if (ModelState.IsValid) {
                 context.Convidados.Add (model);
                 await context.SaveChangesAsync ();
