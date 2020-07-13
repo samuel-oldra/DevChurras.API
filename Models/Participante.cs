c741907607c5c1258d54b8098ca9dca8fa5e8913
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-13 17:12:02 -0300

    Alterado cálculo para diferenciar valor dos consumidores de bebida alcoólica

--- Models/Participante.cs
+++ Models/Participante.cs
@@ -9,5 +9,8 @@ namespace minhaApiWeb.Models {
         [MaxLength (60, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres")]
         [MinLength (3, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres")]
         public string Nome { get; set; }
+
+        [Required (ErrorMessage = "Esse campo é obrigatório")]
+        public bool ConsomeBebidaAlcoolica { get; set; }
     }
 }
