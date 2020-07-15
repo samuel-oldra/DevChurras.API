0adb0481f12cf1677fbfbd9e395bcee1b341d572
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-15 00:00:00 -0300

    Adicionadas imagens para o arquivo README.md #6

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -3,18 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace minhaApiWeb.Data {
     public class DataContext : DbContext {
-        public DataContext (DbContextOptions<DataContext> options) : base (options) {
-
-        }
-
-        /*
-        protected override void OnModelCreating (ModelBuilder modelBuilder) {
-            modelBuilder.Entity<Participante> ()
-                .HasOne<Convidado> (p => p.Convidado)
-                .WithOne (c => c.Participante)
-                .HasForeignKey<Convidado> (c => c.ParticipanteRef);
-        }
-        */
+        public DataContext (DbContextOptions<DataContext> options) : base (options) { }
 
         public DbSet<Participante> Participantes { get; set; }
         public DbSet<Convidado> Convidados { get; set; }
