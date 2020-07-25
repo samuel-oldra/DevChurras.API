4923c626332cf638aea8a7e3aad413a514aaa362
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-25 00:00:00 -0300

    Adicionado SWAGGER ao projeto #1

--- DevChurras.API/Startup.cs
+++ DevChurras.API/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DevChurras.API.Data;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace DevChurras.API
 {
@@ -23,6 +25,21 @@ namespace DevChurras.API
             services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("dados"));
             services.AddScoped<DataContext, DataContext>();
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "DevChurras.API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Samuel B. Oldra",
+                        Email = "samuel.oldra@gmail.com",
+                        Url = new Uri("https://github.com/samuel-oldra")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,6 +59,14 @@ namespace DevChurras.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = string.Empty;
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
             });
         }
     }
