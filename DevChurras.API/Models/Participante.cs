d3645d053159603be9fe882072d17f79d3c6c5bc
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-07-26 19:00:00 -0300

    Padronização #4

--- DevChurras.API/Models/Participante.cs
+++ DevChurras.API/Models/Participante.cs
@@ -8,8 +8,8 @@ namespace DevChurras.API.Models
         public int ParticipanteId { get; set; }
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
-        [MaxLength(60, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres")]
         [MinLength(3, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres")]
+        [MaxLength(60, ErrorMessage = "Esse campo deve conter entre 3 e 60 caracteres")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "Esse campo é obrigatório")]
