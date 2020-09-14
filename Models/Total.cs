b4f748b8593fab69c5c0b43029a47f160bcf7cda
Samuel B. Oldra <samuel.oldra@gmail.com>
2020-09-14 10:32:09 -0300

    Reformatado e reorganizado código

--- Models/Total.cs
+++ Models/Total.cs
@@ -1,27 +1,33 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace minhaApiWeb.Models {
-    public class Total {
+namespace minhaApiWeb.Models
+{
+    public class Total
+    {
         public double TotalArrecadado { get; set; }
 
-        [Required (ErrorMessage = "Este campo é obrigatório")]
-        [Range (0, int.MaxValue, ErrorMessage = "Valor inválido")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0, int.MaxValue, ErrorMessage = "Valor inválido")]
         public double TotalGastoComida { get; set; }
 
-        [Required (ErrorMessage = "Este campo é obrigatório")]
-        [Range (0, int.MaxValue, ErrorMessage = "Valor inválido")]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0, int.MaxValue, ErrorMessage = "Valor inválido")]
         public double TotalGastoBebida { get; set; }
 
-        public double TotalGasto {
-            get {
-                return Math.Round (this.TotalGastoComida + this.TotalGastoBebida, 2);
+        public double TotalGasto
+        {
+            get
+            {
+                return Math.Round(this.TotalGastoComida + this.TotalGastoBebida, 2);
             }
         }
 
-        public double Saldo {
-            get {
-                return Math.Round (this.TotalArrecadado - (this.TotalGastoComida + this.TotalGastoBebida), 2);
+        public double Saldo
+        {
+            get
+            {
+                return Math.Round(this.TotalArrecadado - (this.TotalGastoComida + this.TotalGastoBebida), 2);
             }
         }
     }
