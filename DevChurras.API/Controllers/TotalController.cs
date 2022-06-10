e303efa99713af20438c8c279889eed2d68e16e0
Samuel B. Oldra <samuel.oldra@gmail.com>
2022-06-10 15:34:02 -0300

    Adicionar comentários no SWAGGER

--- DevChurras.API/Controllers/TotalController.cs
+++ DevChurras.API/Controllers/TotalController.cs
@@ -1,5 +1,6 @@
 using DevChurras.API.Data;
 using DevChurras.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -10,8 +11,27 @@ namespace DevChurras.API.Controllers
     [Route("api/total")]
     public class TotalController : ControllerBase
     {
+        // POST: api/total
+        /// <summary>
+        /// Cálculo dos Totais
+        /// </summary>
+        /// <remarks>
+        /// Requisição:
+        /// {
+        ///     "valorGastoComida": 150,
+        ///     "valorGastoBebida": 60
+        /// }
+        /// </remarks>
+        /// <param name="model">Dados dos Valores</param>
+        /// <returns>Objeto criado</returns>
+        /// <response code="200">Sucesso</response>
+        /// <response code="400">Dados inválidos</response>
         [HttpPost]
-        public async Task<IActionResult> Post([FromServices] DataContext context, [FromBody] Total model)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post(
+            [FromServices] DataContext context,
+            [FromBody] Total model)
         {
             var valorPorPessoa = 40;
             var valorPorPessoaQueNaoBebe = 30;
