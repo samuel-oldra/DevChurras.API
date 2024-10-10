cf9f2252a79102619970032916c40fc27a97515d
Samuel B. Oldra <samuel.oldra@gmail.com>
2024-10-10 00:00:00 -0300

    Atualização de pacotes

--- DevChurras.API/Data/DataContext.cs
+++ DevChurras.API/Data/DataContext.cs
@@ -5,9 +5,9 @@ namespace DevChurras.API.Data
 {
     public class DataContext : DbContext
     {
-        public DbSet<Participante> Participantes { get; set; }
-
         public DbSet<Convidado> Convidados { get; set; }
+
+        public DbSet<Participante> Participantes { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     }
